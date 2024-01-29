61bf057f0a5f06dc0cfcd7ed885a9026f1175a82
deepGIVAn <deepakpvt26@gmail.com>
2024-01-29 15:49:20 +0530

    warranty updates

--- controllers/warranty.controller.js
+++ controllers/warranty.controller.js
@@ -1,24 +1,28 @@
 const User = require("../models/user.model");
+const Warranty = require("../models/warranty.model");
+const ProductKey = require("../models/productkey.model");
 
-const getallWarranties = async (req,res) => {
+const getallWarranties = async (req, res) => {
+  const warranties = await Warranty.find().lean();
+  if (!warranties?.length) {
+    return res.status(400).json({ message: "No Warranties Found!!" });
+  }
+  return res.json(warranties);
+};
 
-}
+const createWarranty = async (req, res) => {};
 
-const createWarranty = async (req,res) => {
+const updateWarranty = async (req, res) => {};
 
-}
+const deleteWarranty = async (req, res) => {};
 
-const updateWarranty = async (req,res) => {
-
-}
-
-const deleteWarranty = async (req,res) => {
-
-}
-
-const getWarranty = async (req,res) => {
-
-}
+const getWarranty = async (req, res) => {
+  const id = req.params.id;
+  const warranty = await Warranty.findById(id).lean();
+  if (!warranty)
+    return res.status(400).json({ message: "Warranty Not Found!" });
+  return res.json(warranty);
+};
 
 module.exports = {
   getallWarranties,
